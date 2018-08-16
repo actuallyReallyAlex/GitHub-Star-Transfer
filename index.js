c1158ea65f0eaa48fc84cc68305f6542d89ca3f0
alexlee-dev <alex@alexlee.site>
2018-08-16 05:19:26 -0700

    Fixes Issue #1. Need to refactor to eliminate circular dependencies

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const inquirer = require('inquirer');
 const gradient = require('gradient-string');
 const clear = require('clear');
 
-const startMenu = require('./lib/mainMenu');
+const mainMenu = require('./lib/mainMenu');
 const settingsMenu = require('./lib/settings/settingsMenu');
 const exitMenu = require('./lib/exitMenu');
+
+console.log({mainMenu: mainMenu});
+console.log({settingsMenu: settingsMenu});
+console.log({exitMenu: exitMenu});
 
 const run = () => {
 	// Print Title Screen
 	console.log(gradient.pastel(figlet.textSync('GitHub Star Transfer')));
 
 	// Show Menu Screen
-	inquirer.prompt(startMenu.actions).then(answer => {
+	inquirer.prompt(mainMenu.actions).then(answer => {
 		// answer is an object with 'mainMenu' as the key and the answer as the value
 		// Set choice variable to the value
-		var choice = answer.mainMenu;
-		switch (choice) {
+		// var choice = answer.mainMenu;
+		switch (answer.mainMenu) {
 			case 'Settings':
 				clear();
 				settingsMenu.displaySettingsPrompt();
@@ -30,7 +34,7 @@ const run = () => {
 				break;
 
 			default:
-				console.log(answer);
+				console.log(answer.mainMenu);
 				break;
 		}
 	}).catch(error => {
