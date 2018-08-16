79f7ec9dc9dcef37099936e79300c51cbda81c7e
alexlee-dev <alex@alexlee.site>
2018-08-16 06:42:14 -0700

    Moved menu navigation to one simple function in helpers

--- index.js
+++ index.js
@@ -1,44 +1,17 @@
 #!/usr/bin/env node
 
-const figlet = require('figlet');
-const inquirer = require('inquirer');
-const gradient = require('gradient-string');
-const clear = require('clear');
-
-const mainMenu = require('./lib/mainMenu');
-const settingsMenu = require('./lib/settings/settingsMenu');
-const exitMenu = require('./lib/exitMenu');
-
-console.log({mainMenu: mainMenu});
-console.log({settingsMenu: settingsMenu});
-console.log({exitMenu: exitMenu});
+const helpers = require('./lib/helpers');
 
 const run = () => {
 	// Print Title Screen
-	console.log(gradient.pastel(figlet.textSync('GitHub Star Transfer')));
+	helpers.printTitle('GitHub Star Transfer');
 
-	// Show Menu Screen
-	inquirer.prompt(mainMenu.actions).then(answer => {
-		// answer is an object with 'mainMenu' as the key and the answer as the value
-		// Set choice variable to the value
-		// var choice = answer.mainMenu;
-		switch (answer.mainMenu) {
-			case 'Settings':
-				clear();
-				settingsMenu.displaySettingsPrompt();
-				break;
-
-			case 'Exit':
-				clear();
-				exitMenu.confirmExit('mainMenu');
-				break;
-
-			default:
-				console.log(answer.mainMenu);
-				break;
-		}
-	}).catch(error => {
-		console.log('Error reached within Main Function.', error.message);
+	// Create Main Menu
+	helpers.createMenu({
+		type: 'list',
+		name: 'mainMenu',
+		message: 'What would you like to do?',
+		choices: ['Copy Stars', helpers.separator(), 'Settings', 'Exit']
 	});
 };
 
